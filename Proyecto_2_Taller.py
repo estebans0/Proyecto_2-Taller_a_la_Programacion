ab0f03d169b6e76a1f4c935b313c45a2a66492a1
[estebans0] <[estebanjs029@gmail.com]>
2021-06-04 20:24:18 -0600

    Inicio repositorio

---
+++ Proyecto_2_Taller.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import datetime
+import random
+
+# Creamos la raíz sobre la que se desarrollará el videojuego. 
+raiz_juego = Tk()
+
+print("Prueba")
+
+raiz_juego.mainloop()
