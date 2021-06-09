003973f09de675d22fdf0dcede7d3946a55a4d2e
Felipe Brenes Ramírez <felijunior03@gmail.com>
2021-06-09 07:30:26 -0600

    Pantallas (tienen funcionalidad, faltan elementos decorativos)

--- Proyecto_2_Taller.py
+++ Proyecto_2_Taller.py
@@ -136,12 +136,208 @@ def funcion_jugar():
     pantalla_seleccion.iconbitmap("Icono_proyecto2.ico")
     pantalla_seleccion.config(cursor = "star")
 
-# Botón perteneciente a la función "funcion_jugar":
+    # Canvas, pantalla de selección:
+    canvas_seleccion = Canvas(pantalla_seleccion, width = 1366, height = 768, bg = "Black")
+    canvas_seleccion.pack()
 
+    # Funciones de los botones en pantalla de selección:
+    # Función del botón "Atrás" de la pantalla de selección:
+    def atras_seleccion():
+        pantalla_seleccion.destroy()
+
+    # Función del botón "About":
+    def complementaria():
+        pantalla_seleccion.deiconify()
+
+        # Pantalla complementaria ("About"):
+        pantalla_comp = Toplevel()
+        pantalla_comp.geometry("1366x768")
+        pantalla_comp.iconbitmap("Icono_proyecto2.ico")
+        pantalla_comp.config(cursor = "star")
+
+        # Canvas, pantalla complementaria:
+        canvas_comp = Canvas(pantalla_comp, width = 1366, height = 768, bg = "Black")
+        canvas_comp.pack()
+
+        # Función del botón "Atras" de la pantalla complementaria:
+        def atras_comp():
+            pantalla_comp.destroy()
+
+        # Función del botón "Ayuda" de la pantalla complementaria:
+        def pantalla_ayuda():
+            pantalla_comp.deiconify()
+
+            # Pantalla "Ayuda":
+            pantalla_de_ayuda = Toplevel()
+            pantalla_de_ayuda.geometry("1366x768")
+            pantalla_de_ayuda.iconbitmap("Icono_proyecto2.ico")
+            pantalla_de_ayuda.config(cursor = "star")
+
+            # Canvas pantalla "Ayuda":
+            canvas_ayuda = Canvas(pantalla_de_ayuda, width = 1366, height = 768, bg = "Black")
+            canvas_ayuda.pack()
+
+            # Función del botón "Atrás" de la pantalla de ayuda:
+            def atras_ayuda():
+                pantalla_de_ayuda.destroy()
+            
+            # Botón "Atrás" de la pantalla de ayuda:
+            Boton_atras_ayuda = Button(canvas_ayuda, text = "Atrás", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = atras_ayuda)
+            Boton_atras_ayuda.place(x = 550, y = 500)
+
+        # Botones de la pantalla complementaria:
+        # Botón de la pantalla de ayuda:
+        Boton_ayuda = Button(canvas_comp, text = "Ayuda", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = pantalla_ayuda)
+        Boton_ayuda.place(x = 550, y = 500)
+
+        # Botón "Atrás" de la pantalla complementaria:
+        Boton_atras_comp = Button(canvas_comp, text = "Atrás", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = atras_comp)
+        Boton_atras_comp.place(x = 650, y = 500)
+        
+    # Función del botón "Puntajes"
+    def mejores_puntajes():
+        pantalla_seleccion.deiconify()
+
+        # Pantalla de mejores puntajes:
+        pantalla_puntajes = Toplevel()
+        pantalla_puntajes.geometry("1366x768")
+        pantalla_puntajes.iconbitmap("Icono_proyecto2.ico")
+        pantalla_puntajes.config(cursor = "star")
+
+        # Canvas, pantalla de mejores puntajes:
+        canvas_puntajes = Canvas(pantalla_puntajes, width = 1366, height = 768, bg = "Black")
+        canvas_puntajes.pack()
+
+        # Función del botón "Atrás" de la pantalla de mejores puntajes:
+        def atras_puntajes():
+            pantalla_puntajes.destroy()
+        
+        # Botón "Atrás" de la pantalla de puntajes:
+        Boton_atras_puntajes = Button(canvas_puntajes, text = "Atrás", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = atras_puntajes)
+        Boton_atras_puntajes.place(x = 550, y = 500)
+    
+    # Función del botón "Niveles" (selección de niveles):
+    def seleccion_niveles():
+        pantalla_seleccion.deiconify()
+
+        # Pantalla de selección de niveles:
+        pantalla_niveles = Toplevel()
+        pantalla_niveles.geometry("1366x768")
+        pantalla_niveles.iconbitmap("Icono_proyecto2.ico")
+        pantalla_niveles.config(cursor = "star")
+
+        # Canvas, pantalla de mejores puntajes:
+        canvas_niveles = Canvas(pantalla_niveles, width = 1366, height = 768, bg = "Black")
+        canvas_niveles.pack()
+
+        # Funciones de los botones de la pantalla de selección de niveles:
+        # Botón "Atrás" de la pantalla de selección de niveles:
+        def atras_niveles():
+            pantalla_niveles.destroy()
+        
+        # Funciones de los botones "Nivel 1", "Nivel 2" y "Nivel 3":
+        # Pantalla del primer nivel:
+        def Nivel_1():
+            # Personalización de la pantalla del primer nivel:
+            pantalla_nivel_1 = Toplevel()
+            pantalla_nivel_1.geometry("1366x768")
+            pantalla_nivel_1.iconbitmap("Icono_proyecto2.ico")
+            pantalla_nivel_1.config(cursor = "star")
+
+            # Canvas del nivel 1 (sobre este se dibujarán los elementos que aparecerán en pantalla):
+            canvas_nivel_1 = Canvas(pantalla_nivel_1, width = 1366, height = 768, bg = "Black")
+            canvas_nivel_1.pack()
+
+            # Escenario (fondo) del primer nivel:
+            # canvas_nivel_1.create_image(0, 0, anchor = NW, image = bg_level1)
+
+            # Función del botón "Atrás" de la pantalla del nivel 1:
+            def atras_nivel_1():
+                pantalla_nivel_1.destroy()
+
+        # Pantalla del segundo nivel:
+        def Nivel_2():
+            # Personalización de la pantalla del segundo nivel:
+            pantalla_nivel_2 = Toplevel()
+            pantalla_nivel_2.geometry("1366x768")
+            pantalla_nivel_2.iconbitmap("Icono_proyecto2.ico")
+            pantalla_nivel_2.config(cursor = "star")
+
+            # Canvas del nivel 2 (sobre este se dibujarán los elementos que aparecerán en pantalla):
+            canvas_nivel_2 = Canvas(pantalla_nivel_2, width = 1366, height = 768, bg = "Black")
+            canvas_nivel_2.pack()
+
+            # Escenario (fondo) del segundo nivel:
+            # canvas_nivel_2.create_image(0, 0, anchor = NW, image = bg_level1)
+
+            # Función del botón "Atrás" de la pantalla del nivel 2:
+            def atras_nivel_2():
+                pantalla_nivel_2.destroy()
+
+        # Pantalla del tercer nivel:
+        def Nivel_3():
+            # Personalización de la pantalla del tercer nivel:
+            pantalla_nivel_3 = Toplevel()
+            pantalla_nivel_3.geometry("1366x768")
+            pantalla_nivel_3.iconbitmap("Icono_proyecto2.ico")
+            pantalla_nivel_3.config(cursor = "star")
+
+            # Canvas del nivel 3 (sobre este se dibujarán los elementos que aparecerán en pantalla):
+            canvas_nivel_3 = Canvas(pantalla_nivel_3, width = 1366, height = 768, bg = "Black")
+            canvas_nivel_3.pack()
+
+            # Escenario (fondo) del tercer nivel:
+            # canvas_nivel_3.create_image(0, 0, anchor = NW, image = bg_level1)
+
+            # Función del botón "Atrás" de la pantalla del nivel 3:
+            def atras_nivel_3():
+                pantalla_nivel_3.destroy()
+
+        # Botones de la pantalla de selección de niveles:
+        # Botón que dirige a la pantalla del primer nivel:
+        Boton_nivel_1 = Button(canvas_niveles, text = "Nivel 1", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = Nivel_1)
+        Boton_nivel_1.place(x = 550, y = 500)
+
+        # Botón que dirige a la pantalla del segundo nivel:
+        Boton_nivel_2 = Button(canvas_niveles, text = "Nivel 2", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = Nivel_2)
+        Boton_nivel_2.place(x = 650, y = 500)
+
+        # Botón que dirige a la pantalla del tercer nivel:
+        Boton_nivel_3 = Button(canvas_niveles, text = "Nivel 3", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = Nivel_3)
+        Boton_nivel_3.place(x = 750, y = 500)
+
+        # Botón "Jugar" de la pantalla de selección (inicia el juego en el nivel 1):
+        Boton_jugar_seleccion = Button(canvas_niveles, text = "Jugar", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = Nivel_1)
+        Boton_jugar_seleccion.place(x = 850, y = 500)
+
+        # Botón "Atrás" de la pantalla de selección:
+        Boton_atras_seleccion = Button(canvas_niveles, text = "Atrás", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = atras_niveles)
+        Boton_atras_seleccion.place(x = 950, y = 500)
+
+
+    # Botones de la pantalla de selección:
+    # Botón de la pantalla complementaria:
+    Boton_complemento = Button(canvas_seleccion, text = "About", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = complementaria)
+    Boton_complemento.place(x = 550, y = 500)
+
+    # Botón de la pantalla de mejores puntajes:
+    Boton_puntajes = Button(canvas_seleccion, text = "Puntajes", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = mejores_puntajes)
+    Boton_puntajes.place(x = 650, y = 500) 
+
+    # Botón de la pantalla de selección de niveles:
+    Boton_niveles = Button(canvas_seleccion, text = "Niveles", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = seleccion_niveles)
+    Boton_niveles.place(x = 750, y = 500)
+
+    # Botón para regresar a la pantalla de inicio:
+    Boton_atras_seleccion = Button(canvas_seleccion, text = "Atrás", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = atras_seleccion)
+    Boton_atras_seleccion.place(x = 850, y = 500)       
+
+
+# Botón "Jugar" de la pantalla incial:
 Boton_jugar = Button(canvas_raiz, text = "Jugar", padx = 10, pady = 5, font = "Impact", relief = "raised", bg = "Purple", command = funcion_jugar)
 Boton_jugar.place(x = 550, y = 500)
 
-# Función del botón "Salir":
+# Función del botón "Salir" de la pantalla inicial:
 def cerrar_juego():
     raiz_juego.destroy()
 
